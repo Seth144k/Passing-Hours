d1f3465b5998f7a32850e508b40fcfcbc02bca3d
Seth144k <sethzisrael12@gmail.com>
2023-03-13 12:30:24 -0700

    minor refactor in src/player.lua

--- src/player.lua
+++ src/player.lua
@@ -1,7 +1,7 @@
 player = {}
 
-function player:load(X, Y)
-    self.collider = world:newRectangleCollider(X, Y, 40, 40)
+function player:load(x, y)
+    self.collider = world:newRectangleCollider(x, y, 40, 40)
     self.collider:setFixedRotation(true)
     self.speed = 200
     self.sanity = self.sanity or "high"
@@ -23,7 +23,7 @@ function player:update(dt)
 
     -- sprinting
     if love.keyboard.isDown("lshift") then
-        self.speed = 300
+        self.speed = 400
     else
         self.speed = 200
     end
